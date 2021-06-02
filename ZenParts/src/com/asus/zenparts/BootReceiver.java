0a6225a03ef7aa19679e8f13c736c775f742b973
travarilo <travarilo@gmail.com>
2021-06-02 20:57:16 +0530

    X00TD: added headphone and mic gain
    Co-Authored-By: Rizky Benggolo <travarilo@gmail.com>

--- ZenParts/src/com/asus/zenparts/BootReceiver.java
+++ ZenParts/src/com/asus/zenparts/BootReceiver.java
@@ -36,6 +36,8 @@ public class BootReceiver extends BroadcastReceiver implements Utils {
     private final String TORCH_2_BRIGHTNESS_PATH = "/sys/devices/soc/800f000.qcom,spmi/spmi-0/" +
             "spmi0-03/800f000.qcom,spmi:qcom,pm660l@3:qcom,leds@d300/leds/led:torch_1/" +
             "max_brightness";
+    private final String HEADPHONE_GAIN_PATH = "/sys/kernel/sound_control/headphone_gain";
+    private final String MICROPHONE_GAIN_PATH = "/sys/kernel/sound_control/mic_gain";
 
     public void onReceive(Context context, Intent intent) {
     
@@ -47,6 +49,11 @@ public class BootReceiver extends BroadcastReceiver implements Utils {
             context.startService(new Intent(context, DiracService.class));
         }
 
+        // GAINS
+        int gain = Settings.Secure.getInt(context.getContentResolver(), DeviceSettings.PREF_HEADPHONE_GAIN, 5);
+        FileUtils.setValue(HEADPHONE_GAIN_PATH, gain + " " + gain);
+        FileUtils.setValue(MICROPHONE_GAIN_PATH, Settings.Secure.getInt(context.getContentResolver(), DeviceSettings.PREF_MICROPHONE_GAIN, 0));
+        
         // Ambient
         context.startService(new Intent(context, SensorsDozeService.class));
         
