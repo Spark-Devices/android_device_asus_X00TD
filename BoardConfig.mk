kernelzru/device_asus

X00TD: Enforce vendor tree presence
A compiled build has no chance of booting without the prebuilt
binaries, so fail the build if those are not present.

Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d

BoardConfig.mk
@@ -210,4 +210,4 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 
 # inherit from the proprietary version
--include vendor/asus/X00TD/BoardConfigVendor.mk
+include vendor/asus/X00TD/BoardConfigVendor.mk

Spark-Devices/android_device_asus_X00TD

Signed-off-by: pkm774 <mprabhat774@gmail.com>

BoardConfig.mk
@@ -273,4 +273,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
 # inherit from the proprietary version
--include vendor/asus/X00T/BoardConfigVendor.mk
+include vendor/asus/X00T/BoardConfigVendor.mk