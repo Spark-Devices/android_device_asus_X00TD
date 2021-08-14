301658eb6f5e384a3d8379fd8bd482535993a954
Thagoo <lohit.565@gmail.com>
2021-08-14 17:46:35 +0530

    X00TD: Fix libqdMetaData package name
    linker  : CANNOT LINK EXECUTABLE "/system/bin/wfdservice": library "libqdMetaData.so" not found: needed by /system/lib/libwfdsm.so in namespace (default)

--- device.mk
+++ device.mk
@@ -261,7 +261,7 @@ PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
     libdisplayconfig.qti.vendor \
     liboverlay \
-    libqdMetaData.system \
+    libqdMetaData \
     libqdMetaData.vendor \
     libvulkan \
     libtinyxml \
