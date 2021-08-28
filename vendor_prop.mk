26813678b6da63a921f04767f66623942e96cf4c
jaichiranjeeva <jaichiranjeevadadi@gmail.com>
2021-08-28 15:04:50 +0530

    X00TD: Update system & vendor props
    - to make blur smoother
    
    Co-Authored-By: SagarMakhar <25933684+SagarMakhar@users.noreply.github.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -276,8 +276,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Native blur
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.latch_unsignaled=0 \
-    debug.sf.disable_backpressure=1 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.disable_backpressure=0 \
     ro.surface_flinger.supports_background_blur=1 \
-    ro.sf.blurs_are_expensive=1 \
-    persist.sys.sf.disable_blurs=1
+    ro.sf.blurs_are_expensive=0
