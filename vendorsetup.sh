8822b2d6596a04e5b0cb2cdb26e685b9d8ca3ddc
jaichiranjeeva <jaichiranjeevadadi@gmail.com>
2021-09-05 13:42:27 +0530

    X00TD: Self kill vendorsetup.sh after its first execution

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,5 @@ rm -rf hardware/qcom-caf/msm8998/media
 git clone https://github.com/pkm774/hardware_qcom-caf_media_msm8998 -b 11 hardware/qcom-caf/msm8998/media
 rm -rf hardware/qcom-caf/msm8998/display
 git clone https://github.com/pkm774/hardware_qcom-caf_display_msm8998 -b 11 hardware/qcom-caf/msm8998/display
+
+rm -rf devi*/a*/X*/vendors*
