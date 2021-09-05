bbff0087ec10ca61e98cc00e1da5329a421b98b6
jaichiranjeeva <jaichiranjeevadadi@gmail.com>
2021-09-05 13:37:35 +0530

    X00TD: vendorsetup: Fetch msm8998 HAL's from pkm774 repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,6 @@
-#
+rm -rf hardware/qcom-caf/msm8998/audio
+git clone https://github.com/pkm774/hardware_qcom-caf_msm8998_audio -b audio-hal.lnx.8.0.r14-rel hardware/qcom-caf/msm8998/audio
+rm -rf hardware/qcom-caf/msm8998/media
+git clone https://github.com/pkm774/hardware_qcom-caf_media_msm8998 -b 11 hardware/qcom-caf/msm8998/media
+rm -rf hardware/qcom-caf/msm8998/display
+git clone https://github.com/pkm774/hardware_qcom-caf_display_msm8998 -b 11 hardware/qcom-caf/msm8998/display
