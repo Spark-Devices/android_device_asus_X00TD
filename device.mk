ea006bfbee75c149cb65ca95cb53eced318d2f7c
Nolen Johnson <johnsonnolen@gmail.com>
2021-09-10 20:05:04 +0530

    X00TD: Remove unused dependencies of WFD
    Change-Id: Ie094a8359f20a25b61df8cb1a455dc436a59e231

--- device.mk
+++ device.mk
@@ -486,13 +486,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libaacwrapper \
-    libmediaextractorservice \
-    libnl \
-    libstagefright_enc_common
-
 #PRODUCT_BOOT_JARS += \
 #    WfdCommon
 
