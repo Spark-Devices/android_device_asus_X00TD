8181e6ad59f888b28fc0f51f9cb2dcd3a74176ac
jaichiranjeeva <jaichiranjeevadadi@gmail.com>
2021-10-26 14:08:28 +0530

    X00TD: vendorsetup: Fetch HAL's compatible with A12

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,17 @@
 rm -rf hardware/qcom-caf/msm8998/audio
-git clone https://github.com/pkm774/hardware_qcom-caf_msm8998_audio -b audio-hal.lnx.8.0.r14-rel hardware/qcom-caf/msm8998/audio
 rm -rf hardware/qcom-caf/msm8998/media
-git clone https://github.com/pkm774/hardware_qcom-caf_media_msm8998 -b 11 hardware/qcom-caf/msm8998/media
 rm -rf hardware/qcom-caf/msm8998/display
-git clone https://github.com/pkm774/hardware_qcom-caf_display_msm8998 -b 11 hardware/qcom-caf/msm8998/display
 
-rm -rf devi*/a*/X*/vendors*
+git clone https://github.com/jaichiranjeeva/platform_hardware_qcom_audio.git -b 12.x-caf-msm8998 hardware/qcom-caf/msm8998/audio
+git clone https://github.com/jaichiranjeeva/platform_hardware_qcom_display.git -b 12.x-caf-msm8998 hardware/qcom-caf/msm8998/display
+git clone https://github.com/jaichiranjeeva/platform_hardware_qcom_media.git -b 12.x-caf-msm8998 hardware/qcom-caf/msm8998/media
+
+#temporay to remove bpf checks
+
+rm -rf system/netd
+rm -rf system/bpf
+
+git clone https://github.com/jaichiranjeeva/platform_system_netd.git -b 12.x system/netd
+git clone https://github.com/jaichiranjeeva/platform_system_bpf.git -b 12.x system/bpf
+
+rm -rf devi*/a*/X*/v*.sh
