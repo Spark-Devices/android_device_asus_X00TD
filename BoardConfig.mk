01287bc20c56830fcb88f52999d28b7c09e6a41d
jaichiranjeeva <jaichiranjeevadadi@gmail.com>
2021-11-09 21:39:45 +0530

    Revert "[TEMP] Introduce minimal SEPolicy"
    This reverts commit b0a5d84102a51978f395d9336e8ac3cafa247889.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -247,12 +247,9 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 VENDOR_SECURITY_PATCH := 2020-12-05
 
 # SELinux
-#include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-#BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/ms
-SELINUX_IGNORE_NEVERALLOWS := true
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble
 BOARD_VNDK_VERSION := current
