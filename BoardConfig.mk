2194e7b7c3f199ac7b8feaa29e05ee2fc34b59ba
jaichiranjeeva <jaichiranjeevadadi@gmail.com>
2021-11-09 22:15:23 +0530

    X00TD: BOARD_PLAT is now depreceated

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -245,7 +245,7 @@ VENDOR_SECURITY_PATCH := 2020-12-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble
